4e17304f75bf0e19628493b008db1f06f90576c1
JCaramantin <jhoan1915@gmail.com>
2023-06-20 16:39:13 -0500

    Update method

--- TeamSize/Controllers/TeamSizeController.cs
+++ TeamSize/Controllers/TeamSizeController.cs
@@ -6,23 +6,19 @@ namespace TeamSize.Controllers
     [Route("[controller]")]
     public class TeamSizeController : ControllerBase
     {
- 
 
-        private readonly ILogger<TeamSizeController> _logger;
-
-        public TeamSizeController(ILogger<TeamSizeController> logger)
+        public TeamSizeController()
         {
-            _logger = logger;
         }
 
         [HttpGet(Name = "GetMaxEqualTeams")]
-        public ActionResult<int> GetMaxEqualTeams([FromQuery] int[] teamSize, [FromQuery] int k)
+        public ActionResult<int> GetMaxEqualTeams([FromQuery] List<int> teamSize, [FromQuery] int k)
         {
             if (teamSize == null || k == null)
             {
                 return BadRequest("Los parámetros no pueden ser nulos.");
             }
-            if (teamSize.Length == 0)
+            if (teamSize.Count == 0)
             {
                 return BadRequest("El arreglo 'teamSize' no puede estar vacío.");
             }
@@ -30,17 +26,45 @@ namespace TeamSize.Controllers
             {
                 return BadRequest("El parámetro 'k' no puede ser cero.");
             }
-            Dictionary<int, int> counter = teamSize.GroupBy(x => x)
-                                                .ToDictionary(x => x.Key, x => x.Count());
-            List<int> keys = counter.Keys.OrderByDescending(x => x).ToList();
-            int result = 0;
 
-            foreach (int key in keys)
+            int mode = CalculateMode(teamSize);
+
+            for (int i = 0; i < k; i++)
             {
-                result = Math.Max(result, counter[key] + Math.Min(k, result));
+                int max = teamSize.Max();
+                teamSize[teamSize.IndexOf(max)] = mode;
+            }
+            return Ok(teamSize.ToArray());
+        }
+
+        private int CalculateMode(List<int> teamSizes)
+        {
+            Dictionary<int, int> frequencyMap = new Dictionary<int, int>();
+
+            foreach (int teamSize in teamSizes)
+            {
+                if (frequencyMap.ContainsKey(teamSize))
+                {
+                    frequencyMap[teamSize]++;
+                }
+                else
+                {
+                    frequencyMap[teamSize] = 1;
+                }
             }
 
-            return result;
+            int mode = teamSizes[0];
+            int maxFrequency = 0;
+
+            foreach (var kvp in frequencyMap)
+            {
+                if (kvp.Value > maxFrequency)
+                {
+                    mode = kvp.Key;
+                    maxFrequency = kvp.Value;
+                }
+            }
+            return mode;
         }
     }
 }
